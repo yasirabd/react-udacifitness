4b2e5c24ba7dd58cd6859e1fd1fb076e3931ff11
yasirabd <sir.yasirabd@gmail.com>
2017-11-20 21:56:32 +0700

    changes for AddEntry-SubmitBtn

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
-import { getMetricMetaInfo } from '../utils/helpers'
+import { View, Text, TouchableOpacity } from 'react-native'
+import { getMetricMetaInfo, timeToString } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
+
+function SubmitBtn ({ onPress }) {
+  return (
+    <TouchableOpacity
+      onPress={onPress}>
+        <Text>SUBMIT</Text>
+    </TouchableOpacity>
+  )
+}
 
 export default class AddEntry extends Component {
   state = {
@@ -12,6 +21,21 @@ export default class AddEntry extends Component {
     swim: 0,
     sleep: 0,
     eat: 0,
+  }
+
+  submit = () => {
+    const key = timeToString()
+    const entry = this.state
+
+    // Update Redux
+
+    this.setState(() => ({ run:0, bike:0, swim:0, sleep:0, eat:0 }))
+
+    // Navigate to home
+
+    // Save to "DB"
+
+    // Clear local notification
   }
 
   increment = (metric) => {
@@ -73,6 +97,7 @@ export default class AddEntry extends Component {
             </View>
           )
         })}
+        <SubmitBtn onPress={this.submit} />
       </View>
     )
   }
