4854ee3added76ee36d7db9de939ebee94c3ab86
yasirabd <sir.yasirabd@gmail.com>
2017-11-22 13:06:59 +0700

    changes for route back to home navigation

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -10,6 +10,7 @@ import { submitEntry, removeEntry } from '../utils/api'
 import { connect } from 'react-redux'
 import { addEntry } from '../actions'
 import { white, purple } from '../utils/colors'
+import { NavigationActions } from 'react-navigation'
 
 function SubmitBtn ({ onPress }) {
   return (
@@ -72,6 +73,7 @@ class AddEntry extends Component {
     this.setState(() => ({ run:0, bike:0, swim:0, sleep:0, eat:0 }))
 
     // Navigate to home
+    this.toHome()
 
     // Save to "DB"
     submitEntry({ key, entry })
@@ -88,9 +90,16 @@ class AddEntry extends Component {
     }))
 
     // Route to home
+    this.toHome()
 
     // Update "DB"
     removeEntry(key)
+  }
+
+  toHome = () => {
+    this.props.navigation.dispatch(NavigationActions.back({
+      key: 'AddEntry',
+    }))
   }
 
   render() {
