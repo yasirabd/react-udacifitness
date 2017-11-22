eeb05983f27516beefd2d609dd9ca1083886cff1
yasirabd <sir.yasirabd@gmail.com>
2017-11-22 12:52:02 +0700

    changes for connect-EntryDetail

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,5 +1,9 @@
 import React , { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import { connect } from 'react-redux'
+import { white } from '../utils/colors'
+import MetricCard from './MetricCard'
+import { timeToString, getDailyReminderValue } from '../utils/helpers'
 
 class EntryDetail extends Component {
   static navigationOptions = ({ navigation }) => {
@@ -10,17 +14,39 @@ class EntryDetail extends Component {
     const day = entryId.slice(8)
 
     return {
-      title: `${day}/${month}/${year}`
+      title: `${month}/${day}/${year}`
     }
   }
 
   render() {
+    const { metrics } = this.props
+
     return (
-      <View>
-        <Text>Entry Detail - {this.props.navigation.state.params.entryId}</Text>
+      <View style={styles.container}>
+        <MetricCard metrics={metrics} />
+        <Text>Entry Detail - {JSON.stringify(this.props.navigation.state.params.entryId)}</Text>
       </View>
     )
   }
 }
 
-export default EntryDetail
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: white,
+    padding: 15,
+  }
+})
+
+function mapStateToProps (state, { navigation }) {
+  const { entryId } = navigation.state.params
+
+  return {
+    entryId,
+    metrics: state[entryId]
+  }
+}
+
+export default connect(
+  mapStateToProps,
+)(EntryDetail)
