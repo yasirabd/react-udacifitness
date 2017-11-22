e77df778710f1889feeda8f501a559b95f6e08d9
yasirabd <sir.yasirabd@gmail.com>
2017-11-22 14:15:17 +0700

    update for Live-setLocation

--- components/Live.js
+++ components/Live.js
@@ -2,16 +2,51 @@ import React, { Component } from 'react'
 import { View, Text, ActivityIndicator, StyleSheet, TouchableOpacity } from 'react-native'
 import { Foundation } from '@expo/vector-icons'
 import { purple, white } from '../utils/colors'
+import { Location, Permissions } from 'expo'
+import { calculateDirection } from '../utils/helpers'
 
 export default class Live extends Component {
   state = {
     coords: null,
-    status: 'granted',
+    status: null,
     direction: '',
+  }
+
+  componentDidMount () {
+    Permissions.getAsync(Permissions.LOCATION)
+      .then(({ status}) => {
+        if (status === 'granted') {
+          return this.setLocation()
+        }
+
+        this.setState(() => ({ status }))
+      })
+      .catch((error) => {
+        console.warn('Error getting location permission: ', error)
+
+        this.setState(() => ({ status: 'undetermined' }))
+      })
   }
 
   askPermission = () => {
 
+  }
+
+  setLocation = () => {
+    Location.watchPositionAsync({
+      enableHighAccuracy: true,
+      timeInterval: 1,
+      distanceInterval: 1,
+    }, ({ coords }) => {
+      const newDirection = calculateDirection(coords.heading)
+      const { direction } = this.state
+
+      this.setState(() => ({
+        coords,
+        status: 'granted',
+        direction: newDirection,
+      }))
+    })
   }
 
   render () {
